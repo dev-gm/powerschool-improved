5f7473cd2708c52a79600c0bc6ac9413d0f67226
Gavin Mason <github@gavinm.us>
2022-01-04 11:51:33 -0500

    Fixed TSC errors

--- dist.js
+++ dist.js
@@ -1,2 +1,28 @@
 "use strict";
-alert(document.location.href);
+function injectLoginPage() {
+    var username_tag = document.getElementById("fieldAccount"), password_tag = document.getElementById("fieldPassword"), submit_btn_tag = document.getElementById("btn-enter-sign-in"), login_raw = localStorage.getItem("powerschool-login");
+    if (username_tag == null || password_tag == null || submit_btn_tag == null)
+        return;
+    if (login_raw != null) {
+        // if login for powerschool in localStorage, enter username, enter password, and submit form
+        var login = JSON.parse(login_raw);
+        username_tag.setAttribute("value", login["username"]);
+        password_tag.setAttribute("value", login["password"]);
+        submit_btn_tag.click();
+    }
+    else {
+        // otherwise, have the manually-entered username and password be stored in localStorage for next time user visits page
+        submit_btn_tag.addEventListener("click", function () {
+            localStorage.setItem("powerschool-login", JSON.stringify({
+                username: username_tag ? username_tag.getAttribute("value") : "",
+                password: password_tag ? password_tag.getAttribute("value") : ""
+            }));
+        });
+        alert("Please enter username and password to allow auto-login to work the next time you visit this page");
+    }
+}
+function injectHomePage() { }
+if (location.href.includes("aps.powerschool.com/public"))
+    injectLoginPage();
+else if (location.href.includes("aps.powerschool.com/guardian"))
+    injectHomePage();
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
 function injectLoginPage() {
 	var username_tag = document.getElementById("fieldAccount"),
 		password_tag = document.getElementById("fieldPassword"),
-		sumbit_btn_tag = document.getElementById("btn-enter-sign-in"),
-		login = JSON.parse(localStorage.getItem("powerschool-login"));
-	if (login && login.username && login.password) {
+		submit_btn_tag = document.getElementById("btn-enter-sign-in"),
+		login_raw = localStorage.getItem("powerschool-login");
+	if (username_tag == null || password_tag == null || submit_btn_tag == null)
+		return;
+	if (login_raw != null) {
 		// if login for powerschool in localStorage, enter username, enter password, and submit form
-		username_tag.value = login.username;
-		password_tag.value = login.password;
-		document.getElementById("btn-enter-sign-in").click("submit");
+		var login = JSON.parse(login_raw);
+		username_tag.setAttribute("value", login["username"]);
+		password_tag.setAttribute("value", login["password"]);
+		submit_btn_tag.click();
 	} else {
 		// otherwise, have the manually-entered username and password be stored in localStorage for next time user visits page
-		document.getElementById("btn-enter-sign-in").addEventListener("click", () => {
+		submit_btn_tag.addEventListener("click", () => {
 			localStorage.setItem("powerschool-login", JSON.stringify({
-				username: username_tag.value,
-				password: password_tag.value,
+				username: username_tag ? username_tag.getAttribute("value") : "",
+				password: password_tag ? password_tag.getAttribute("value") : "",
 			}));
 		});
 		alert("Please enter username and password to allow auto-login to work the next time you visit this page");

6d380ac725a94cc840ebcdd812d2a88dd351a9b3
Gavin Mason <github@gavinm.us>
2022-01-06 16:24:33 -0500

    Changed bookmarklet code

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,12 +2,7 @@ javascript: (() => {
 	if (!document.baseURI.includes("aps.powerschool.com"))
 		location.href = "https://aps.powerschool.com/";
 	fetch("https://raw.githubusercontent.com/dev-gm/powerschool-improved/main/dist.js")
-		.then(response => response.text())
-		.then(code => {
-			let script_tag = document.createElement("script");
-			script_tag.setAttribute("text", code);
-			script_tag.setAttribute("id", "powerschool-improved-script");
-			document.body.appendChild(script_tag);
-		})
-		.catch(err => alert(`Failed to load code\n${err}`));
+		.then(res => res.text())
+		.then(eval)
+		.catch(err => alert("Failed to load code\n" + err));
 })();
